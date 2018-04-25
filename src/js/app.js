e8723ca829984636e1c3c9f62093fd66f6aa1486
Aidan <aidanschmitt47@gmail.com>
2018-04-24 23:00:00 -0500

    Added basic POST example

--- src/js/app.js
+++ src/js/app.js
@@ -15,4 +15,27 @@ app.controller('testController', function($scope, $http){
         });
     };
 
+    $scope.postFeed = function() {
+        var data = {
+            'user': $scope.username,
+            'message': $scope.feedMessage
+        };
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8;'
+            }
+        }
+
+        $http({
+            method: 'POST',
+            url: 'http://127.0.0.1:5000/postFeed',
+            data: data
+        }).then(function successCallback(response, status, headers, config) {
+            $scope.feed = 'User ' + response.data.user + '\'s message ' + response.data.message + ' was posted!';
+        }, function errorCallback(response, status, headers, config) {
+            $scope.feed = 'ERROR\n\n' + response;
+        });
+    }
+
 });
